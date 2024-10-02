9e42ee742d3a5aad671b21fe7accdca0e307d8f5
Neto Menezes <61053569+consultor360@users.noreply.github.com>
2024-10-01 23:01:59 -0300

    Add files via upload

--- views/pipelines/view.php
+++ views/pipelines/view.php
@@ -9,20 +9,35 @@ defined('BASEPATH') or exit('No direct script access allowed'); ?>
             <div class="col-md-12">
                 <div class="panel_s">
                     <div class="panel-body">
-                        <h4 class="no-margin"><?php echo $pipeline->name; ?></h4>
+                        <h4 class="no-margin"><?php echo _l('pipelines_list'); ?></h4>
                         <hr class="hr-panel-heading" />
-                        <div class="row">
-                            <div class="col-md-6">
-                                <h5><?php echo _l('pipeline_details'); ?></h5>
-                                <p><strong><?php echo _l('pipeline_name'); ?>:</strong> <?php echo $pipeline->name; ?></p>
-                                <p><strong><?php echo _l('pipeline_description'); ?>:</strong> <?php echo $pipeline->description; ?></p>
-                            </div>
-                            <div class="col-md-6">
-                                <h5><?php echo _l('pipeline_stages'); ?></h5>
-                                <?php foreach($pipeline->stages as $stage): ?>
-                                    <p><?php echo $stage->name; ?></p>
-                                <?php endforeach; ?>
-                            </div>
+                        <div class="table-responsive">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th><?php echo _l('pipeline_name'); ?></th>
+                                        <th><?php echo _l('leads_count'); ?></th>
+                                        <th><?php echo _l('actions'); ?></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach($pipelines as $pipeline): ?>
+                                    <tr>
+                                        <td><?php echo $pipeline->name; ?></td>
+                                        <td>
+                                            <?php
+                                            $leads_count = $this->db->where('pipeline_id', $pipeline->id)->count_all_results('tblleads');
+                                            echo $leads_count;
+                                            ?>
+                                        </td>
+                                        <td>
+                                            <a href="<?php echo admin_url('multi_pipeline/pipelines/edit/' . $pipeline->id); ?>" class="btn btn-default btn-sm"><i class="fa fa-pencil"></i></a>
+                                            <a href="<?php echo admin_url('multi_pipeline/pipelines/delete/' . $pipeline->id); ?>" class="btn btn-danger btn-sm" onclick="return confirm('<?php echo _l('are_you_sure'); ?>');"><i class="fa fa-trash"></i></a>
+                                        </td>
+                                    </tr>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
